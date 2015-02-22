b3dbdbf7aa48d0bcd82747e8bf2ae5f89764e37b
unknown <C:\Users\Administrator\AppData\Roaming\The Bat!\Mail>
2015-02-22 16:47:06 +0500

    first commit

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var translit = require('../translit');
+
+function example() {
+ var a = "Съешь ещё этих мягких французских булок, да выпей же чаю!",
+ //  var a = "мірь",          // Старославянский
+ b = translit(a,  5);
+ c = translit(b, -5);
+ console.log("Транслируемая строка:\n%s\nТранслированная строка:\n%s\nОбратная трансляция:\n%s", a, b, c);
+}
+
+example();
+
